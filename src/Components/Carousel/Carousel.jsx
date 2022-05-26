4eb8e71c0ad03a3578cd418b1279625f5ed782ae
Antony-LE <le.antony.l@gmail.com>
2022-05-26 23:28:00 +0200

    Add modification on data flow

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 import React, { useState } from 'react';
+import axios from 'axios';
+import uniqid from 'uniqid';
 
 // Importation des modules Swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -19,11 +21,37 @@ function Carousel() {
   // Création d'un state permettant de stocker tous les utilisateurs dans un tableau
   const [{ users }, setUsers] = useState({ users: [] });
 
+  // Création des states permettant le stockage des données d'un utilisateur
+  const [userPicture, setUserPicture] = useState('');
+  const [userLastname, setUserLastname] = useState('');
+  const [userFirstname, setUserFirstname] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userDob, setUserDob] = useState('');
+
   /* Fontion permettant d'ajouter une carte utilisateur dans le tableau users
   et de récupérer son nouvel état sans le spread operator un nouveau tableau sera
   généré à chaque fois */
-  const addUser = () => {
-    users.push(<div key={users.length}><UserCard /></div>);
+
+  const addUser = async () => {
+    // Récupération et stockage des données à partir de l'API
+    const response = await axios.get('https://randomuser.me/api/');
+    setUserPicture(response.data.results[0].picture.large);
+    setUserFirstname(response.data.results[0].name.first);
+    setUserLastname(response.data.results[0].name.last);
+    setUserEmail(response.data.results[0].email);
+    setUserDob((response.data.results[0].dob.date).slice(0, 10));
+    // Je push un nouveau user dans le tableau users à chaque click
+    users.push(
+      <div>
+        <UserCard
+          Picture={userPicture}
+          Lastname={userLastname}
+          Firstname={userFirstname}
+          Email={userEmail}
+          Dob={userDob}
+        />
+      </div>,
+    );
     setUsers({ users: [...users] });
   };
 
@@ -33,12 +61,10 @@ function Carousel() {
       {users.length === 0
         ? (
           <div className="swiper-container">
-            <SwiperSlide>
-              <div className="instruction">Please click on the below button to add new users !</div>
-              <button type="button" onClick={addUser}>
-                <AddIcon />
-              </button>
-            </SwiperSlide>
+            <div className="instruction">Please click on the below button to add new users !</div>
+            <button type="button" onClick={addUser}>
+              <AddIcon />
+            </button>
           </div>
         )
         : (
@@ -61,7 +87,7 @@ function Carousel() {
               {/* On map sur chaque itération du tableau user
               pour afficher dynamiquement les cards */}
               { users.map((user) => (
-                <SwiperSlide>
+                <SwiperSlide key={uniqid()}>
                   {user}
                 </SwiperSlide>
               ))}
--- src/Components/UserCard/UserCard.jsx
+++ src/Components/UserCard/UserCard.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+/* eslint-disable react/prop-types */
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // Importation des modules MUI
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import EmailIcon from '@mui/icons-material/Email';
 import CakeIcon from '@mui/icons-material/Cake';
@@ -13,61 +13,53 @@ import Divider from '@mui/material/Divider';
 // importation du fichier de style
 import './UserCard.css';
 
-function UserCard() {
-  // Création des states permettant le stockage des données d'un utilisateur
-  const [userPicture, setUserPicture] = useState('');
-  const [userLastname, setUserLastname] = useState('');
-  const [userFirstname, setUserFirstname] = useState('');
-  const [userEmail, setUserEmail] = useState('');
-  const [userDob, setUserDob] = useState('');
-
-  // Requête axios permettant de recevoir aléatoirement les données d'un utlisateur
-  const getUserData = async () => {
-    const response = await axios.get('https://randomuser.me/api/');
-    setUserPicture(response.data.results[0].picture.large);
-    setUserFirstname(response.data.results[0].name.first);
-    setUserLastname(response.data.results[0].name.last);
-    setUserEmail(response.data.results[0].email);
-    setUserDob((response.data.results[0].dob.date).slice(0, 10));
-  };
-
-  /* J'utilise un useEffect pour éviter que les données de la card ne se
-   raffraichissent en continue */
-  useEffect(() => {
-    getUserData();
-  }, []);
-
+function UserCard({
+  // Props récupérées dans le composant parent Carousel
+  Picture, Lastname, Firstname, Email, Dob,
+}) {
   return (
     <div className="card-container">
       <Card sx={{ maxWidth: 300, maxHeight: 345 }}>
-        <img src={userPicture} alt={userPicture} />
+        <img src={Picture} alt={Picture} />
         <CardContent>
           <Typography gutterBottom variant="h12" component="div">
-            {userLastname}
+            {Lastname}
           </Typography>
           <Typography gutterBottom variant="h12" color="text.secondary">
-            {userFirstname}
+            {Firstname}
           </Typography>
           <Divider />
           <Typography variant="body2" fontSize="small" color="text.secondary">
-            <p>
-              <EmailIcon fontSize="small" />
-              <br />
-              {userEmail}
-            </p>
+            <EmailIcon fontSize="small" />
+            <br />
+            {Email}
           </Typography>
           <Divider />
           <Typography variant="body2" fontSize="small" color="text.secondary">
-            <p>
-              <CakeIcon fontSize="small" />
-              <br />
-              {userDob}
-            </p>
+            <CakeIcon fontSize="small" />
+            <br />
+            {Dob}
           </Typography>
         </CardContent>
       </Card>
     </div>
   );
 }
+
+UserCard.propTypes = {
+  Picture: PropTypes.string,
+  Lastname: PropTypes.string,
+  Firstname: PropTypes.string,
+  Email: PropTypes.string,
+  Dob: PropTypes.string,
+};
+
+UserCard.defaultProps = {
+  Picture: '',
+  Lastname: '',
+  Firstname: '',
+  Email: '',
+  Dob: '',
+};
 
 export default UserCard;
